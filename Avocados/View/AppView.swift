1c21a11d668478405084697ad1447df9f9d36b69
Tin Tran <tinhardware@gmail.com>
2022-06-11 15:15:08 +0700

    Add a tab view

---
+++ Avocados/View/AppView.swift
@@ -0,0 +1,47 @@
+//
+//  AppView.swift
+//  Avocados
+//
+//  Created by Tin Tran on 11/06/2022.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            AvocadosView()
+                .tabItem({
+                    Image("tabicon-branch")
+                    Text("Avocados")
+                })
+            ContentView()
+                .tabItem({
+                    Image("tabicon-book")
+                    Text("Recipes")
+                })
+            AvocadosView()
+                .tabItem({
+                    Image("tabicon-avocado")
+                    Text("Ripening")
+                })
+            AvocadosView()
+                .tabItem({
+                    Image("tabicon-settings")
+                    Text("Settings")
+                })
+        } //: TABVIEW
+        .edgesIgnoringSafeArea(.top)
+        .accentColor(.primary)
+    }
+}
+
+// MARK: - PREVIEW
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
---
+++ Avocados/View/AvocadosView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadosView.swift
+//  Avocados
+//
+//  Created by Tin Tran on 11/06/2022.
+//
+
+import SwiftUI
+
+struct AvocadosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AvocadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadosView()
+    }
+}
