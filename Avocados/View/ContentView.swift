28086211c3363e1181fdf382dd332551d72e0cbc
Tin Tran <tinhardware@gmail.com>
2022-06-11 16:06:21 +0700

    Add HeaderView into ContentView

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // MARK: - HEADER
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                // MARK: - FOOTER
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                } //: VSTACK
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            } //: VSTACK
+        } //: SCROLLVIEW
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
